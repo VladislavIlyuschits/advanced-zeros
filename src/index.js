a5d8fa5ae5607f863e847d7ba88ef0e6b61c63b5
Vladislav <ilyuschitsvlad@gmail.com>
2018-03-07 12:48:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+function callPrime(n, k) 
+	{
+		var res = 0;
+		while (n > 0) 
+		{
+			n = Math.floor(n / k);
+			res += n;
+		}
+		return res;
+	}
+
+module.exports = function getZerosCount(number, base) 
+	{
+	var a = [];
+	var b = base;
+		for(var i = 2; i <= Math.sqrt(base); i++)
+		{
+			if(b % i != 0) continue;
+			var p = 0;
+			while(b % i == 0) 
+				{ 
+					p++; b /= i 
+				};
+			var cur = Math.floor(callPrime(number, i) / p);
+			a.push(cur);
+		}
+		if(b > 1) a.push(callPrime(number, b));
+		return Math.min( ...a );
+	}
